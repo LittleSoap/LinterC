901eb55f157f8734b3a4883849ff0fe413cf0670
Stephane <hollanderstephane5@gmail.com>
2018-11-28 23:50:04 +0100

    Function comma_spacing

--- Linter/prototypes.h
+++ Linter/prototypes.h
@@ -6,3 +6,4 @@ char* readConfFile(FILE * defaultFile, char* LineRead);
 int writeConfFile(FILE* defaultConfFile);
 int max_file_line_number(int n, char* test);
 int max_line_numbers(int n, char * test);
+int comma_spacing(char * test);
